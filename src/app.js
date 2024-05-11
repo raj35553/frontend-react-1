3eb68361a26ee14e3e51d04483b40cdfd9557875
Mohit Chauhan <mohit2991kumar@gmail.com>
2024-05-11 10:36:49 +0530

    Impliment JSX

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/app.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-const app = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "This is h1 tag."),
-    React.createElement("h2", { id: "heading" }, "Mohit"),
-    React.createElement("h3", { id: "heading" }, "This is h3 tag.11dedsew"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "This is h1 tag of child 2.333"),
-    React.createElement("h2", { id: "heading" }, "This is h2 tag of child 212222."),
-  ]),
-]);
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div>
+          <img src="../public/react.jpeg" />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(app);
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Footer = () => {
+  return <div id="footer">Footer</div>;
+};
+export default Footer;
