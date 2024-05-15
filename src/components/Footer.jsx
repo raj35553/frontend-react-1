7053e668c8519887ecc4e677d172bad230ee4f0d
rajkumar <raj.kumar@passiongaming.in>
2024-05-15 18:34:54 +0530

    chnage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,51 @@
 import React from "react";
 
-const Footer = () => {
-  function clickMe() {
+
+  function Footer() {
     // console.log(">>>>>>>>>> mohit");
-  }
+ 
 
   return (
-    <div id="footer">
-      Footer<button onClick={clickMe}>Click Me Now Dev</button>
+    <footer id="footer">
+    <div className="container-fluid" >
+          <div className="container">
+            
+                <div className="footer-one">
+                  Got a show, event, activity or a great experience? Partner with us & get listed on BookMyShow
+                </div>
+          </div>
     </div>
+    <div className="container-fluid footer-two">
+          <div className="container">
+              <div className=" d-flex justify-content-around">
+                  <div class="d-flex justify-content-between align-items-center justify-content-center">
+                      <img src="" />
+                      <p>24/7 CUSTOMER CARE</p>
+                    
+                  </div>
+                  <div class="d-flex justify-content-between align-items-center justify-content-center">
+                      <img src="" />
+                      <p>24/7 CUSTOMER CARE</p>
+                    
+                  </div>
+                  <div class="d-flex justify-content-between align-items-center justify-content-center">
+                      <img src="" />
+                      <p>24/7 CUSTOMER CARE</p>
+                    
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div className="container-fluid">
+          <div className="container">
+              <div className="footer-three d-flex justify-content-around">
+               
+              </div>
+          </div>
+        </div>
+      
+    
+    </footer>
   );
 };
 export default Footer;
