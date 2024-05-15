44e7240419590e6bb181bc2263697edfb505be43
rajkumar <raj.kumar@passiongaming.in>
2024-05-15 18:32:55 +0530

    chnage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from '../images/logo.png';
 
 function Header() {
-  return <nav class="navbar bg-body-tertiary" aria-label="First navbar example">
+  return <nav class="navbar" aria-label="First navbar example">
   <div class="container">
   <div class="d-flex justify-content-start align-items-center">
       <a class="navbar-brand" href="#"><img src={logo} alt={"logo"}/> </a>
@@ -11,7 +11,7 @@ function Header() {
         </form>
     </div>
    <div class="d-flex justify-content-start align-items-center">
-        <button type="button" class="btn btn-danger me-2">Sign in</button>
+        <button type="button" class="btn btn-danger btn-sm me-2">Sign in</button>
         
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarDark" aria-controls="offcanvasNavbarDark" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -30,22 +30,9 @@ function Header() {
             <li class="nav-item">
               <a class="nav-link" href="#">Link</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
+           
           </ul>
-          <form class="d-flex mt-3" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+        
         </div>
       </div>
   </div>
