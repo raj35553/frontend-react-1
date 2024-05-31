1f8ed5bc65769bc654237ecd09abb956fd507d22
rajkumar <raj.kumar@passiongaming.in>
2024-05-31 10:43:33 +0530

    change

--- src/components/Recomand-movie.jsx
+++ src/components/Recomand-movie.jsx
@@ -4,7 +4,9 @@ import Recomandimage from '../images/movie.png';
 function RecomandMovie() {
   return <div className="container mt-4"><h1>Recommended Movies</h1>
   <div className=""> 
-  <img src={Recomandimage} alt={"Recomandimage"}/>
+     <img src={Recomandimage} alt={"Recomandimage"}/>
+     <h3>Movie Name</h3>
+     <p>Type</p>
   </div>
   </div>;
 }
